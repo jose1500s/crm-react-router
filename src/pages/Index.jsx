309c45d6c8285182540175a6f9fbd1dfc7bd062f
Jose Antonio P <120035990@upq.edu.mx>
2024-01-27 00:26:05 -0600

    Layout creado, index y nuevo cliente, Link creado

---
+++ src/pages/Index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+    return (
+        <>
+            <h2>clientes</h2>
+        </>
+    )
+}
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,7 @@
+export default function NuevoCliente() {
+    return (
+        <>
+            <h2>nuevo cliente</h2>
+        </>
+    )
+}
